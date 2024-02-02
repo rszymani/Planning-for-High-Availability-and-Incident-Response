d1fd284ecfdb6505e4c69be40e1c4726f99581eb
rafal.szymaniuk <rszymaniuk@gmail.com>
2024-02-02 13:17:16 +0100

    chore(lesson4): add protocol ingress

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/modules/ec2/ec2.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/modules/ec2/ec2.tf
@@ -16,10 +16,12 @@ resource "aws_security_group" "ec2_sg" {
  ingress {
   from_port = "80"
   to_port = "80"
+  protocol = "tcp"
  }
  ingress {
    from_port = "22"
    to_port = "22"
+   protocol = "tcp"
  }
  tags = {
   Name = "ec2_sg"
