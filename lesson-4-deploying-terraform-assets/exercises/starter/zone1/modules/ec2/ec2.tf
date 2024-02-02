139f9d8388920e18109218bc7686772569cab1b0
rafal.szymaniuk <rszymaniuk@gmail.com>
2024-02-02 13:21:26 +0100

    chore(lesson4): change to correct variables

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/modules/ec2/ec2.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/modules/ec2/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ubuntu" {
-  ami           = var.ami_aws
-  count = var.count
+  ami           = var.aws_ami
+  count = var.instance_count
   instance_type = var.instance_type
   subnet_id = var.public_subnet_ids[0]
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
