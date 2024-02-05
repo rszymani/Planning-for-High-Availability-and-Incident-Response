f82176efc13bdd96a6c4b754fa58787e34e9ac05
rafal.szymaniuk <rszymaniuk@gmail.com>
2024-02-05 20:21:47 +0100

    chore(lesson5): change aurora version

--- lesson-5-rds-deploy/exercises/starter/modules/rds-p/rds.tf
+++ lesson-5-rds-deploy/exercises/starter/modules/rds-p/rds.tf
@@ -31,7 +31,7 @@ resource "aws_rds_cluster" "udacity_cluster" {
   db_subnet_group_name     = aws_db_subnet_group.udacity_db_subnet_group.name
   engine                   = "aurora-mysql"
   engine_mode              = "provisioned"
-  engine_version           = "5.7.mysql_aurora.2.10.1"
+  engine_version           = "5.7.mysql_aurora.2.11.1"
   skip_final_snapshot      = true
   storage_encrypted        = false
   depends_on = [aws_rds_cluster_parameter_group.cluster_pg]
