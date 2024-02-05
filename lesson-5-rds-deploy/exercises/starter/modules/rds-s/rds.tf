f61d0f2c2d95846df3d641f0ed11376854ab7d11
rafal.szymaniuk <rszymaniuk@gmail.com>
2024-02-05 20:15:53 +0100

    chore(lesson5): change aurora version

--- lesson-5-rds-deploy/exercises/starter/modules/rds-s/rds.tf
+++ lesson-5-rds-deploy/exercises/starter/modules/rds-s/rds.tf
@@ -46,7 +46,7 @@ resource "aws_rds_cluster_instance" "udacity_instance-s" {
   instance_class       = "db.t2.small"
   db_subnet_group_name = aws_db_subnet_group.udacity_db_subnet_group.name
   engine               = aws_rds_cluster.udacity_cluster-s.engine
-  engine_version       = aws_rds_cluster.udacity_cluster.engine_version
+  engine_version       = aws_rds_cluster.udacity_cluster-s.engine_version
 }
 
 resource "aws_security_group" "db_sg_2" {
